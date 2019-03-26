1af9f3e43af60831467d4e39d15a4e3bd516f848
Joe Hoyle <joehoyle@gmail.com>
2019-03-26 16:53:10 +0700

    Add stream DB driver and modifications

--- inc/namespace.php
+++ inc/namespace.php
@@ -16,47 +16,14 @@ function on_plugins_loaded() {
 		require_once ROOT_DIR . '/vendor/humanmade/require-login/plugin.php';
 	}
 	if ( $config['audit-log'] ) {
-		add_filter( 'wp_stream_admin_menu_title', function () : string {
-			return __( 'Audit Log' );
-		} );
-		add_filter( 'wp_stream_admin_page_title', function () : string {
-			return __( 'Audit Log Records' );
-		} );
-		/**
-		 * Filter the network wide activated plugin to include stream
-		 *
-		 * This will not actually get included (as the path doesn't exist)
-		 * but this is the only way to trigger Stream's Network Admin configuration
-		 * option.
-		 */
-		add_filter( 'site_option_active_sitewide_plugins', function ( $plugins ) : array {
-			$plugins['stream/stream.php'] = true;
-			return $plugins;
-		} );
-
-		add_filter( 'site_option_wp_stream_network', __NAMESPACE__ . '\\default_stream_network_options' );
-		add_filter( 'default_site_option_wp_stream_network', __NAMESPACE__ . '\\default_stream_network_options' );
-
-		add_action( 'netdwork_admin_menu', function () {
-			/**
-			 * @var \Stream\Plugin
-			 */
-			global $wp_stream;
-			remove_submenu_page( $wp_stream->admin->records_page_slug, $wp_stream->admin->network->network_settings_page_slug );
-		}, 11 );
-		require_once ROOT_DIR . '/vendor/humanmade/stream/stream.php';
+		require_once __DIR__ . '/stream/namespace.php';
+		Stream\bootstrap();
 	}
 
 	if ( ! empty( $config['2-factor-authentication'] ) ) {
 		add_filter( 'two_factor_providers', __NAMESPACE__ . '\\remove_2fa_dummy_provider' );
 		require_once ROOT_DIR . '/vendor/humanmade/two-factor/two-factor.php';
 	}
-}
-
-function default_stream_network_options( $options ) : array {
-	$options['general_site_access'] = 0;
-	$options['keep_records_indefinitely'] = true;
-	return $options;
 }
 
 /**
---
+++ inc/stream/namespace.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace HM\Platform\Security\Stream;
+
+use const HM\Platform\ROOT_DIR;
+
+function bootstrap() {
+	add_filter( 'wp_stream_admin_menu_title', function () : string {
+		return __( 'Audit Log' );
+	} );
+	add_filter( 'wp_stream_admin_page_title', function () : string {
+		return __( 'Audit Log Records' );
+	} );
+	/**
+	 * Filter the network wide activated plugin to include stream
+	 *
+	 * This will not actually get included (as the path doesn't exist)
+	 * but this is the only way to trigger Stream's Network Admin configuration
+	 * option.
+	 */
+	add_filter( 'site_option_active_sitewide_plugins', function ( $plugins ) : array {
+		$plugins['stream/stream.php'] = true;
+		return $plugins;
+	} );
+
+	add_filter( 'site_option_wp_stream_network', __NAMESPACE__ . '\\default_stream_network_options' );
+	add_filter( 'default_site_option_wp_stream_network', __NAMESPACE__ . '\\default_stream_network_options' );
+	add_filter( 'wp_stream_db_driver', __NAMESPACE__ . '\\set_stream_db_driver' );
+	add_action( 'network_admin_menu', function () {
+		/**
+		 * @var \Stream\Plugin
+		 */
+		global $wp_stream;
+		remove_submenu_page( $wp_stream->admin->records_page_slug, $wp_stream->admin->network->network_settings_page_slug );
+	}, 11 );
+	require_once ROOT_DIR . '/vendor/humanmade/stream/stream.php';
+}
+
+function default_stream_network_options( $options ) : array {
+	$options['general_site_access'] = 0;
+	$options['general_keep_records_indefinitely'] = true;
+	return $options;
+}
+
+function set_stream_db_driver( string $db_driver ) : string {
+	return __NAMESPACE__ . '\\CloudWatch_Driver';
+}
