15a2792fa8cd3d5c98f9a318438eda12014250ee
Robert O'Rourke <robert@o-rourke.org>
2020-02-17 12:34:17 +0000

    split out register callback before function guard

--- load.php
+++ load.php
@@ -8,12 +8,7 @@ use function Altis\register_module;
 require_once __DIR__ . '/inc/namespace.php';
 require_once __DIR__ . '/inc/passwords/namespace.php';
 
-// Don't self-initialize if this is not an Altis execution.
-if ( ! function_exists( 'add_action' ) ) {
-	return;
-}
-
-add_action( 'altis.modules.init', function () {
+function register() {
 	$default_settings = [
 		'enabled'                   => true,
 		'require-login'             => ! in_array( get_environment_type(), [ 'production', 'local' ], true ),
@@ -22,4 +17,11 @@ add_action( 'altis.modules.init', function () {
 		'minimum-password-strength' => 2,
 	];
 	register_module( 'security', __DIR__, 'Security', $default_settings, __NAMESPACE__ . '\\bootstrap' );
-} );
+}
+
+// Don't self-initialize if this is not an Altis execution.
+if ( ! function_exists( 'add_action' ) ) {
+	return;
+}
+
+add_action( 'altis.modules.init', __NAMESPACE__ . '\\register' );
